7942d0977e56a8a4ee2b8feaf6a1a9be8d6fc011
Rudra Patel <patelrudra2003@gmail.com>
2023-10-07 18:35:32 -0400

    Add report form and API

---
+++ src/app/api/parking-spots/route.ts
@@ -0,0 +1,51 @@
+import clientPromise from "@/lib/mongodb";
+import { NextRequest } from "next/server";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const data = body.request;
+  try {
+    const client = await clientPromise;
+    const db = client.db("ParkUTK");
+    const reports = db.collection("reports");
+
+    reports.insertOne({
+      parkingGarage: data.parkingGarage,
+      date: data.parkingGarage,
+      range: data.range,
+    });
+
+    return Response.json({ success: true, payload: data });
+  } catch (e) {
+    return Response.json({ success: false, payload: e });
+    console.log(e);
+  }
+}
+
+interface Report {
+  parkingGarage: string;
+  date: string;
+  range: string;
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    const client = await clientPromise;
+    const db = client.db("ParkUTK");
+    const reports = db.collection("reports");
+
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("garage");
+
+    let response;
+    if (query) {
+      response = await reports.find({ parkingGarage: query }).toArray();
+    } else {
+      response = await reports.find().toArray();
+    }
+
+    return Response.json({ success: true, payload: response });
+  } catch (e) {
+    return Response.json({ success: false, payload: e });
+  }
+}
--- src/app/report-parking/page.tsx
+++ src/app/report-parking/page.tsx
@@ -1,5 +1,109 @@
+"use client";
+
+import { useState } from "react";
+
 export default function ReportParking() {
-    return (
-        <h1>Report Parkin</h1>
-    )
+  const parkingGarages = [
+    "11th Street Garage (G13)",
+    "Administration Garage (G3/G4/G5)",
+    "Lake Avenue Garage (G11)",
+    "Locust Street Parking Garage",
+    "McClung Plaza Garage (G1/G2)",
+    "Neyland Drive Garage (G10)",
+    "Pratt Pavilion (G14)",
+    "Terrace Avenue Parking Garage (G17)",
+    "Volunteer Boulevard Parking Garage(G16)",
+    "Volunteer Hall (G15)",
+    "West Campus Parking Garage (G7)",
+    "White Avenue Parking Garage (G12)",
+  ];
+
+  const ranges = ["0 - 5", "5 - 10", "10 - 20", "20 - 40", "40+"];
+
+  const [selectedParkingGarage, setSelectedParkingGarage] = useState<string>(
+    "11th Street Garage (G13)"
+  );
+  const [selectedRange, setSelectedRange] = useState<string>("0 - 5");
+
+  const handleParkingGarageChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSelectedParkingGarage(event.target.value);
+  };
+
+  const handleRangeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedRange(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const request = {
+      parkingGarage: selectedParkingGarage,
+      date: new Date().toISOString(),
+      range: selectedRange,
+    };
+
+    fetch("/api/parking-spots", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ request }),
+    });
+  };
+
+  const findAll = async () => {
+    const response = await fetch("/api/parking-spots");
+    const body = await response.json();
+    console.log(body.payload[1].report);
+  };
+
+  const find = async (parkingGarage: string) => {
+    const response = await fetch(`/api/parking-spots?garage=${parkingGarage}`);
+    const body = await response.json();
+  };
+
+  return (
+    <main>
+      <h1>Report Parking</h1>
+
+      <form onSubmit={handleSubmit}>
+        <select
+          value={selectedParkingGarage}
+          onChange={handleParkingGarageChange}
+          className="text-black"
+        >
+          {parkingGarages.map((garage, index) => {
+            return (
+              <option value={garage} key={index}>
+                {garage}
+              </option>
+            );
+          })}
+        </select>
+
+        <select
+          value={selectedRange}
+          onChange={handleRangeChange}
+          className="text-black"
+        >
+          {ranges.map((range) => {
+            return <option value={range}>{range}</option>;
+          })}
+        </select>
+
+        <input type="submit" />
+      </form>
+
+      <h1>Current parking garage: {selectedParkingGarage}</h1>
+      <h1>Current range: {selectedRange}</h1>
+
+      <div>
+        <button onClick={findAll}>CLICK TO GET ALL REPORTS</button>
+      </div>
+      <div>
+        <button onClick={() => find(selectedParkingGarage)}>
+          FIND TO GET CURRENT GARAGE'S REPORTS
+        </button>
+      </div>
+    </main>
+  );
 }
