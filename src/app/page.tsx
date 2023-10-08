55466d719685e68a1431b811a0d2c8ba942cc92c
TrishuPatel <trishujpatel23@gmail.com>
2023-10-07 22:32:56 -0400

    Fix merge conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-<<<<<<< HEAD
-      <h1>Park UTK</h1>
-      <h2>Created by Tennessee Trio: Kaylee Bae, Rudra Patel, and Trishu Patel</h2>
-      <Link href="/find-parking">Find Parking</Link>
-      <br></br>
-      <Link href="/report-parking">Report Parking</Link>
-=======
       <hr className="h-2 bg-[#FF8200] border-0"></hr>
       <h1 className="text-white bg-[#313e48] text-3xl px-8 py-2">Park UTK</h1>
       <h2 className="text-[#313e48] text-xl px-8 py-2">
@@ -27,7 +20,6 @@ export default function Home() {
       >
         Report Parking
       </Link>
->>>>>>> fc9c3eeaea3d2508eea2404eb09836a565590c4d
     </main>
   );
 }
