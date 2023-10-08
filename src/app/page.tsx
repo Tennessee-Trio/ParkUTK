29881511862337fd10f0126bde7b080e701729ca
6u9 <6u9@ornl.gov>
2023-10-08 17:47:20 -0500

    Minor aesthetic stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,19 @@ export default function Home() {
   return (
     <main>
       <hr className="h-2 bg-[#FF8200] border-0"></hr>
-      <h1 className="text-white bg-[#313e48] text-3xl px-8 py-2 cursor-pointer"><Link href={"/"}>Park UTK</Link></h1>
-      <h2 className="text-[#313e48] text-xl px-8 py-2">
+      <h1 className="text-white bg-[#313e48] text-4xl px-8 py-12 cursor-pointer"><Link href={"/"}>Park UTK</Link></h1>
+      <h2 className="text-[#313e48] text-l px-8 py-2">
         Created by Tennessee Trio
       </h2>
       <Link
         href="/find-parking"
-        className="text-[#313e48] text-3xl px-8 py-2 block"
+        className="text-[#FF8200] text-2xl px-8 py-2 block"
       >
         Find Parking
       </Link>
       <Link
         href="/report-parking"
-        className="text-[#313e48] text-3xl px-8 py-2 block"
+        className="text-[#FF8200] text-2xl px-8 py-2 block"
       >
         Report Parking
       </Link>
