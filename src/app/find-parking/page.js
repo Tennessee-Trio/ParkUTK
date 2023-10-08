d94c04457748a1aba3690f4d9a16f2f86910e0e2
Rudra Patel <patelrudra2003@gmail.com>
2023-10-08 15:45:06 -0400

    Add styling

--- src/app/find-parking/page.js
+++ src/app/find-parking/page.js
@@ -39,44 +39,49 @@ export default function FindParking() {
 
   if (data) {
     return (
-      <MapContainer
-        className="h-[500px]"
-        center={[35.9572666073227, -83.92894490123355]}
-        zoom={16}
-        scrollWheelZoom={false}
-      >
-        {data.map((recentResponse, index) => {
-          const information = recentResponse.payload;
-          const formattedTime = dayjs(information.date).format("h:mma");
-          const formattedDate = dayjs(information.date).format("DD/MM");
-          return (
-            <Marker
-              position={[information.latitude, information.longitude]}
-              icon={
-                new Icon({
-                  iconUrl: "/marker-icon.png",
-                  iconSize: [25, 41],
-                  iconAnchor: [12, 41],
-                })
-              }
-              key={index}
-            >
-              <Popup>
-                <h1 className="text-xl">{information.parkingGarage}</h1>
-                <p>
-                  The most recent report is from {formattedTime} on{" "}
-                  {formattedDate}.
-                </p>
-                <p>There are {information.range} parking spots available.</p>
-              </Popup>
-            </Marker>
-          );
-        })}
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
+      <>
+        <hr className="h-2 bg-[#FF8200] border-0"></hr>
+        <h1 className="text-white bg-[#313e48] text-3xl px-8 py-2">Park UTK</h1>
+        <MapContainer
+          className="h-[calc(100vh-60px)]"
+          center={[35.9572666073227, -83.92894490123355]}
+          zoom={16}
+          scrollWheelZoom={false}
+        >
+          {data.map((recentResponse, index) => {
+            const information = recentResponse.payload;
+            const formattedTime = dayjs(information.date).format("h:mma");
+            const formattedDate = dayjs(information.date).format("DD/MM");
+            return (
+              <Marker
+                position={[information.latitude, information.longitude]}
+                icon={
+                  new Icon({
+                    iconUrl: "/marker-icon.png",
+                    iconSize: [25, 41],
+                    iconAnchor: [12, 41],
+                    popupAnchor: [1, -30],
+                  })
+                }
+                key={index}
+              >
+                <Popup>
+                  <h1 className="text-xl">{information.parkingGarage}</h1>
+                  <p>
+                    The most recent report is from {formattedTime} on{" "}
+                    {formattedDate}.
+                  </p>
+                  <p>There are {information.range} parking spots available.</p>
+                </Popup>
+              </Marker>
+            );
+          })}
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </>
     );
   }
 }
