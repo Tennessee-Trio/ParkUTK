fc9c3eeaea3d2508eea2404eb09836a565590c4d
TrishuPatel <trishujpatel23@gmail.com>
2023-10-07 21:47:37 -0400

    Stylized the interface

--- src/app/find-parking/page.tsx
+++ src/app/find-parking/page.tsx
@@ -1,5 +1,10 @@
 export default function FindParking() {
     return (
-        <h1>Find Parking</h1>
+        <main>
+            <hr className="h-2 bg-[#FF8200] border-0"></hr>
+            <h1 className="text-white bg-[#313e48] text-3xl px-8 py-2">Park UTK</h1>
+            <h1 className="br-white text-[#313e48] text-3xl px-8 py-2">Find Parking</h1>
+        </main>
+
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,23 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Park UTK</h1>
-      <h2>Created by Tennessee Trio</h2>
-      <Link href="/find-parking">Find Parking</Link>
-      <Link href="/report-parking">Report Parking</Link>
+      <hr className="h-2 bg-[#FF8200] border-0"></hr>
+      <h1 className="text-white bg-[#313e48] text-3xl px-8 py-2">Park UTK</h1>
+      <h2 className="text-[#313e48] text-xl px-8 py-2">
+        Created by Tennessee Trio
+      </h2>
+      <Link
+        href="/find-parking"
+        className="text-[#313e48] text-3xl px-8 py-2 block"
+      >
+        Find Parking
+      </Link>
+      <Link
+        href="/report-parking"
+        className="text-[#313e48] text-3xl px-8 py-2 block"
+      >
+        Report Parking
+      </Link>
     </main>
   );
 }
